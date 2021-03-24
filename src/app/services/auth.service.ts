ac3ab502ef25f17ec4bbe6dd1485728e9b29af27
MadRavingMan <48089175+MadRavingMan@users.noreply.github.com>
2021-03-24 13:09:04 +0200

    implemented view model of filled evaluation form (#6)

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Teacher } from '../shared/teacher';
 
 @Injectable({
@@ -7,19 +7,15 @@ import { Teacher } from '../shared/teacher';
 })
 export class AuthService {
   //TODO: remove testTeacher after auth implementation
-  testTeacher: Teacher = {
-    id: 456846,
-    name: 'Marcia',
-    surname: 'David',
-    username: 'David89',
-    password: '6048d820b9693e2936a907d2',
-  };
-
-  private authTeacher: BehaviorSubject<Teacher> = new BehaviorSubject(
-    this.testTeacher
-  );
-
-  getCurrentUser(): Teacher {
-    return { ...this.authTeacher.value };
+  getCurrentUser(): Observable<Teacher> {
+    const testTeacher: Teacher = {
+      id: 456846,
+      name: 'Marcia',
+      surname: 'David',
+      username: 'David89',
+      password: '6048d820b9693e2936a907d2',
+      fullName: 'Marcia Davi'
+    };
+    return of(testTeacher);
   }
 }
