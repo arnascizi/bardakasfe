f4884880dfcaed080d54637fc8f915b8ee75a3ef
Ernestas <ernestas.okunevic@gmail.com>
2021-03-26 01:59:00 +0200

    Implemented student list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { StudentDetailsComponent } from './student-details/student-details.component';
 import { TeachersPageComponent } from './teachers-page/teachers-page.component';
 import { EvaluationFormComponent } from './evaluation-form/evaluation-form.component';
 import { StudentDropdownComponent } from './student-dropdown/student-dropdown.component';
+import { StudentsPageComponent } from './students-page/students-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
@@ -17,10 +17,11 @@ const routes: Routes = [
     children: [
       { path: 'evaluation', component: EvaluationFormComponent },
       { path: 'evaluation/:id', component: EvaluationFormComponent },
-      { path: 'students', component: StudentDetailsComponent },
+      { path: 'students', component: StudentsPageComponent },
       { path: 'teachers', component: TeachersPageComponent },
       { path: 'teachers/:id', component: TeacherDetailsComponent },
       { path: 'dropdown', component: StudentDropdownComponent },
+      { path: 'dropdown/:id', component: StudentDropdownComponent },
     ],
   },
   { path: 'login', component: LoginComponent },
